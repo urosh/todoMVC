b904bac08b2031eff1219e2908b393ee7e5c3cea
urosh <uros.damnjanovic@gmail.com>
2014-02-26 16:55:29 +0200

    Adding views file, they look most complex so far. Lets hope all is well after. By the way i am using Handlebars template engine.

---
+++ js/views/todos.js
@@ -0,0 +1,41 @@
+// js/views/todos.js
+
+var app = app || {};
+
+// Todo Item View
+
+// The DOM element for a todo item... 
+app.TodoView = Backbone.View.extend({
+
+	// .... is a list tag
+	tagName: 'li',
+
+	// Cache the template function for a single item
+	template: Handlebars.compile($("#item-template").html()),
+
+	// The DOM events specific to an item
+	events:{
+		'dblclick label': 'edit',
+		'keypress .edit': 'updateOnEnter',
+		'blur .edit': 'close'
+	},
+
+	initialize: function(){
+		this.listenTo(this.model, 'change', this.render);
+	},
+
+	render: function(){
+		this.$el.html(this.template(this.model.toJSON() ));
+		this.$input = this.$('.edit');
+		return this;
+	},
+
+	edit: function(){
+		this.$el.addClass('editing');
+		this.$input.focus();
+	},
+
+	
+	
+
+})
