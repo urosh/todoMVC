3fae8e3b99ed4154d80fc12e37f04df6bf75126b
urosh <uros.damnjanovic@gmail.com>
2014-02-26 14:34:27 +0200

    Adding todo model.

---
+++ js/models/todo.js
@@ -0,0 +1,22 @@
+//if app is defined app is app if not app is empty object.
+var app = app || {};
+
+// Todo Model
+// --------------
+// Our basic **Todo** model has 'title', 'order', and 'completed' attributes.
+
+app.Todo = Backbone.Model.extend({
+
+	//Default attributes ensure that each todo created has 'title' and "completed" keys
+	defaults; {
+		title: '',
+		completed: false
+	},
+
+	//Toggle the 'completed' state of this todo item.
+	toggle: function(){
+		this.save({
+			completed: !this.get('completed');
+		})
+	}
+});
